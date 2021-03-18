c104f127365007f4faa8e98030baa78f8abe969b
Stefano <tepo@hotmail.it>
2021-03-18 16:00:25 +0100

    Aggiunta possibilità di eliminare elementi dalla lista

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // Come abbiamo fatto questa mattina insieme dovete creare una todo list che permatta di aggiungere (tramite campo input) e rimuovere gli elementi!
+
+$(function () {
+// 1. permetto di rimuovere gli elementi cliccando sulla X
+$(".to_do_list").on("click", ".far", function() {
+  $(this).parent().remove();
+});
+
+
+});
